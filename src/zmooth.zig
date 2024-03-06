12a9e6ba11fc363a68753da5d896349ea62bb9b4
adworacz <561689+adworacz@users.noreply.github.com>
2024-03-06 14:09:20 -0800

    Add zmooth.zig, which will be the entry point.
    However, I can't use it yet since recursive imports are a problem.
    
    I need to update to the latest version of the Vapoursynth-zig bindings
    to fix.

---
+++ src/zmooth.zig
@@ -0,0 +1,9 @@
+const vapoursynth = @import("vapoursynth");
+const vs = vapoursynth.vapoursynth4;
+
+const temporalMedian = @import("temporal_median.zig");
+
+export fn VapourSynthPluginInit2(plugin: *vs.Plugin, vspapi: *const vs.PLUGINAPI) void {
+    _ = vspapi.configPlugin.?("com.adub.zmooth", "zmooth", "Smoothing functions in Zig", vs.makeVersion(1, 0), vs.VAPOURSYNTH_API_VERSION, 0, plugin);
+    _ = vspapi.registerFunction.?("TemporalMedian", "clip:vnode;radius:int:opt;planes:int[]:opt;", "clip:vnode;", temporalMedian.temporalMedianCreate, null, plugin);
+}

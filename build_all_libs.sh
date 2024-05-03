897d92211455324f8ba97bffbf0e7c55d78a78dc
adworacz <561689+adworacz@users.noreply.github.com>
2024-05-03 08:33:43 -0700

    Update build script.
    Adds Znver4 target and emit sha256 sums.

--- build_all_libs.sh
+++ build_all_libs.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-rm -rf zig-out
-rm -f zsmooth.avx2.dll
-rm -f zsmooth.avx512.dll
+set -eu
+
+rm -rf zig-out zsmooth.avx2.* zsmooth.znver4.*
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=x86_64_v3
 cp zig-out/lib/zsmooth.dll zsmooth.avx2.dll 
 
-zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=x86_64_v4
-cp zig-out/lib/zsmooth.dll zsmooth.avx512.dll 
+zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=znver4
+cp zig-out/lib/zsmooth.dll zsmooth.znver4.dll 
+
+zip -9 zsmooth.avx2.dll.zip zsmooth.avx2.dll
+zip -9 zsmooth.znver4.dll.zip zsmooth.znver4.dll
+
+sha256sum zsmooth*.dll* > zsmooth_checksums.sha256

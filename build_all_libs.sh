e72637dd3bf37cb79fd069bb85015686a333b363
adworacz <561689+adworacz@users.noreply.github.com>
2025-01-19 06:59:50 -0800

    Build into dedicated directory for cleanliness

--- build_all_libs.sh
+++ build_all_libs.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 set -eu
 
-rm -rf zig-out zsmooth.avx2.* zsmooth.znver4.*
+rm -rf zig-out build
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=x86_64_v3
-cp zig-out/lib/zsmooth.dll zsmooth.avx2.dll 
+cp zig-out/lib/zsmooth.dll build/zsmooth.avx2.dll 
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=znver4
-cp zig-out/lib/zsmooth.dll zsmooth.znver4.dll 
+cp zig-out/lib/zsmooth.dll build/zsmooth.znver4.dll 
 
+pushd build
 zip -9 zsmooth.avx2.dll.zip zsmooth.avx2.dll
 zip -9 zsmooth.znver4.dll.zip zsmooth.znver4.dll
 
 sha256sum zsmooth*.dll* > zsmooth_checksums.sha256
+popd

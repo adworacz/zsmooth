2229a928e6b787c66a0577528320037563f8d4d1
adworacz <561689+adworacz@users.noreply.github.com>
2025-04-27 16:04:57 -0700

    Fix typo in string.zig.

--- src/common/string.zig
+++ src/common/string.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 /// the vapoursynth C API.
 ///
 /// Note that passing these strings back to Vapoursynth effectively means that
-/// we're going to *leak memory when the program exits, but there's nothing we
+/// we're going to *leak# memory when the program exits, but there's nothing we
 /// can do about that due to the realities of the C interop.
 pub fn printf(allocator: std.mem.Allocator, comptime fmt: []const u8, args: anytype) []const u8 {
     return std.fmt.allocPrintZ(allocator, fmt, args) catch "Out of memory occurred while writing string.";

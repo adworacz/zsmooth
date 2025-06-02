6162a311416d409d48b55bef9140bbbeceaceb18
adworacz <561689+adworacz@users.noreply.github.com>
2025-06-01 18:48:01 -0700

    Fix `zig build test` with optimize float options.

--- build.zig
+++ build.zig
@@ -53,6 +53,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     lib_unit_tests.root_module.addImport("vapoursynth", vapoursynth_dep.module("vapoursynth"));
+    lib_unit_tests.root_module.addOptions("config", options);
     lib_unit_tests.linkLibC();
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);

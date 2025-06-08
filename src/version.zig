7c8908385ba9d32068aea705c4aca607c6c8211f
adworacz <561689+adworacz@users.noreply.github.com>
2025-06-08 11:19:40 -0700

    Bump to version 0.11

--- src/version.zig
+++ src/version.zig
@@ -1,3 +1,3 @@
 const std = @import("std");
 // Expose a version that's easily consumable throughout the project.
-pub const version = std.SemanticVersion{ .major = 0, .minor = 10, .patch = 0 };
+pub const version = std.SemanticVersion{ .major = 0, .minor = 11, .patch = 0 };
